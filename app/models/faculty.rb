4c1ef0b5d09a6a37d9cedbefbf0386eba55430e4
ronak-dattani-simform <ronak.dattani@simformsolutions.com>
2023-02-07 20:52:08 +0530

    TE7-T115:ror_learning:Feat:Active Record Callback Task

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -4,4 +4,36 @@ class Faculty < ApplicationRecord
   validates :email, confirmation: true, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
   validates :designation, inclusion: { in: %w(Prof Ass.Prof), message: "Designation not valid" }
   validates :designation, exclusion: { in: %w(HOD Sr.Prof), message: "Designation not allowed." }
+
+  after_create :email_validate
+  
+  after_initialize do |faculty|
+    puts "You have initialized a faculty !"
+  end
+
+  after_find do |faculty|
+    puts "You have found a faculty !"
+  end
+
+  after_create do |faculty|
+    puts "You have create a faculty !"
+  end 
+
+  after_destroy do |faculty|
+    puts "faculty object has been destroyed."
+  end
+
+  after_update do |faculty|
+    puts "faculty has been updated !"
+  end
+
+    private 
+  
+  def email_validate
+    puts "Email Validated."
+  end 
+  
+
+
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,55 @@ class Student < ApplicationRecord
                                       message: "Department can't be %{value}" }
 
   validates :terms, acceptance: true
+
+  after_create :send_welcome_mail unless: :skip_sending_welcome_email
+  #when we want to skip this call back, we can save skip_sending_welcome_email = true
+  before_create :check_length
+  before_validation :check_date
+  after_validation :confirm_date
+  after_initialize do |student|
+    puts "You have initialized a Student !"
+  end
+
+  after_find do |student|
+    puts "You have found a Student !"
+  end
+
+  after_create do |student|
+    puts "You have create a Student !"
+  end 
+
+  after_destroy do |student|
+    puts "Student object has been destroyed."
+  end
+
+  after_update do |student|
+    puts "Student has been updated !"
+  end
+
+  after_touch do |student|
+    puts "You have touched a student."
+  end
+
+
+  private 
+
+  def check_date
+    puts "Date Chacked."
+  end
+
+  def confirm_date 
+    puts "Date checked after validation."
+  end
+
+  def check_length
+    if first_name.length > 2 
+        return true
+    else
+        errors.add( :base, "name length should be more then 2 letter.")
+        puts "name length should be more then 2 letter."
+
+    end 
+  end
+
 end
